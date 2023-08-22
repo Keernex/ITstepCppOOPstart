bcfb50685a86a05652552976abbdaa5cd0de399b
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-08-22 11:42:50 +0300

    no done

--- DroneEngine.h
+++ DroneEngine.h
@@ -2,20 +2,20 @@
 
 class DroneEngine
 {
-    vector<Drone> ShowDrone()
-	{
-        cout << "Name: " << name << endl;
-        cout << "Size: " << size << endl;
-        cout << "Type: " << type << endl;
-        cout << "Ready model: " << ready_model << endl;
-        cout << "Max speed: " << max_speed << endl;
-        cout << "Control type: " << control_type << endl;
-        cout << "Engine type: " << engine_type << endl;
-        cout << "Charging time: " << charging_time << endl;
-        cout << "Flight time: " << flight_time << endl;
-        cout << "Flight height: " << flight_height << endl;
-        cout << "Weight: " << weight << endl;
-        return 0;
-	}
+ //   void ShowDrone()
+	//{
+ //       cout << "Name: " << name << endl;
+ //       cout << "Size: " << size << endl;
+ //       cout << "Type: " << type << endl;
+ //       cout << "Ready model: " << ready_model << endl;
+ //       cout << "Max speed: " << max_speed << endl;
+ //       cout << "Control type: " << control_type << endl;
+ //       cout << "Engine type: " << engine_type << endl;
+ //       cout << "Charging time: " << charging_time << endl;
+ //       cout << "Flight time: " << flight_time << endl;
+ //       cout << "Flight height: " << flight_height << endl;
+ //       cout << "Weight: " << weight << endl;
+ //       return 0;
+	//}
 };
 
